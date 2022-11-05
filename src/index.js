a0509fb726b1205f9c0e59b75c92810652919d6a
uchario <uchario@yahoo.com>
2022-11-05 21:14:29 +0100

    feat: add react-router-dom and createBrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Root from './routes/root';
+import Contact from './routes/contact';
+import Main from './components/Main';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root/>,
+    children: [
+      {
+        path: "/",
+        element: <Main/>,
+      },
+      {
+        path: "/contact",
+        element: <Contact/>,
+      }
+    ]
+  },
+]);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
