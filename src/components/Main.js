7bd58865f3f9c1d02af7dd11c76eeb46989406cd
uchario <uchario@yahoo.com>
2022-11-05 21:23:31 +0100

    feat: add Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,7 @@
 import { Fragment, useState } from 'react';
 
+import { Link } from 'react-router-dom';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShare, } from '@fortawesome/free-solid-svg-icons'
 import { faSlack, faGithub } from '@fortawesome/free-brands-svg-icons';
@@ -14,13 +16,8 @@ let twitterLink = "https://twitter.com/aritarant",
     zuriPython = "https://books.zuri.team/python-for-beginners?ref_id=arich",
     zuriPitch = "https://background.zuri.team",
     zuriDesign = "https://books.zuri.team/design-rules",
-    zuriInternship = "https://internship.zuri.team/",
-    ingressive = "https://ingressive.org/",
     gitHub = "https://github.com/",
-    slack = "https://slack.com",
-    ingressiveImage = "https://i0.wp.com/ingressive.org/wp-content/uploads/2020/05/I4G-Logo-Color-Cropped.png?fit=2225%2C539&ssl=1",
-    zuriImage = "https://internship.zuri.team/static/media/logo.36d2d48a.svg";
-
+    slack = "https://slack.com";
 
 const Main = () => {
     const [image, setImage] = useState(profileImage);
@@ -41,7 +38,7 @@ const Main = () => {
                             <FontAwesomeIcon icon={faShare} />
                         </span>
                     </div>
-                    <h4>Annette Black</h4>
+                    <h4>Arichinho Tarantula</h4>
                     <p hidden>Arich</p>
                 </section>
                 <section className={`${styles.links}`}>
@@ -51,21 +48,13 @@ const Main = () => {
                     <p><a href={zuriPython} id="book__python" target="_blank" rel='noreferrer'>Python Books</a></p>
                     <p><a href={zuriPitch} id="pitch" target="_blank" rel='noreferrer'>Backgroud Check for Coders</a></p>
                     <p><a href={zuriDesign} id="book__design" target="_blank" rel='noreferrer'>Design Books</a></p>
+                    <p><Link to={`contact`}>Contact Me</Link></p>
                 </section>
                 <section className={`${styles.icons}`}>
                     <a href={slack} target="_blank" rel='noreferrer'><FontAwesomeIcon icon={faSlack}/></a>
                     <a href={gitHub} target="_blank" rel='noreferrer'><FontAwesomeIcon icon={faGithub}/></a>
                 </section>
             </div>
-
-            
-
-            <footer className={`${styles.footer}`}>
-                <a href={zuriInternship} target="_blank" rel='noreferrer'><img src={zuriImage} alt="Hello world"/></a>
-                <span>HNG Internship 9 Frontend Task</span>
-                <a href={ingressive} target="_blank" rel='noreferrer'><img src={ingressiveImage} alt="Hello world"/></a>
-            </footer> 
-
         </Fragment>
     );
 }
